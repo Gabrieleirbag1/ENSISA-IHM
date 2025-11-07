4532eca1be16fc9de11fb00a12f05be84b4faf4e
Gabrieleirbag1 <gabrielgarrone670@gmail.com>
2025-11-07 11:20:07 +0100

    feat: restructure application views and controllers, replacing primary with main

--- TD1/src/main/java/ensisa/birds/MainController.java
+++ TD1/src/main/java/ensisa/birds/MainController.java
@@ -3,7 +3,7 @@ package ensisa.birds;
 import java.io.IOException;
 import javafx.fxml.FXML;
 
-public class PrimaryController {
+public class MainController {
 
     @FXML
     private void switchToSecondary() throws IOException {
--- TD1/src/main/java/ensisa/birds/SecondaryController.java
+++ TD1/src/main/java/ensisa/birds/SecondaryController.java
@@ -6,7 +6,7 @@ import javafx.fxml.FXML;
 public class SecondaryController {
 
     @FXML
-    private void switchToPrimary() throws IOException {
-        BirdApplication.setRoot("primary");
+    private void switchToMain() throws IOException {
+        BirdApplication.setRoot("main");
     }
 }
