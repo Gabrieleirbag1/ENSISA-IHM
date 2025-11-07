918fcd9cf1e3c9beb434a866b32cf54540feed27
Gabrieleirbag1 <gabrielgarrone670@gmail.com>
2025-11-07 13:54:18 +0100

    feat: update MainController and main.fxml to display bird names

--- TD1/src/main/java/ensisa/birds/MainController.java
+++ TD1/src/main/java/ensisa/birds/MainController.java
@@ -2,20 +2,37 @@ package ensisa.birds;
 
 import java.io.IOException;
 
+import ensisa.birds.model.Bird;
 import ensisa.birds.model.BirdRepository;
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
 
 public class MainController {
+
+    @FXML
+    private Label latinNameLabel;
+
+    @FXML
+    private Label commonNameLabel;
+
+    public Bird currentBird;
 
     private BirdRepository repository;
 
     public MainController() {
         repository = new BirdRepository();
         repository.load();
+        currentBird = repository.birds.get(0);
+    }
+
+    public void initialize() {
+        commonNameLabel.setText(currentBird.getCommonName());
+        latinNameLabel.setText(currentBird.getLatinName());
     }
 
     @FXML
     private void switchToSecondary() throws IOException {
         BirdApplication.setRoot("secondary");
     }
+
 }
