d0023c4c7458165f56e5ae6c2157fdd7aff7bfd9
Gabrieleirbag1 <gabrielgarrone670@gmail.com>
2025-11-07 11:35:35 +0100

    feat: add Bird model class and update dependencies for JSON processing

--- TD1/src/main/java/ensisa/birds/MainController.java
+++ TD1/src/main/java/ensisa/birds/MainController.java
@@ -1,9 +1,18 @@
 package ensisa.birds;
 
 import java.io.IOException;
+
+import ensisa.birds.model.BirdRepository;
 import javafx.fxml.FXML;
 
 public class MainController {
+
+    private BirdRepository repository;
+
+    public MainController() {
+        repository = new BirdRepository();
+        repository.load();
+    }
 
     @FXML
     private void switchToSecondary() throws IOException {
--- TD1/src/main/java/module-info.java
+++ TD1/src/main/java/module-info.java
@@ -1,7 +1,8 @@
 module ensisa.birds {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    opens ensisa.birds to javafx.fxml;
-    exports ensisa.birds;
-}
+ requires transitive javafx.controls;
+ requires javafx.fxml;
+ requires com.fasterxml.jackson.databind;
+ opens ensisa.birds to javafx.fxml, com.fasterxml.jackson.databind;
+ exports ensisa.birds;
+ exports ensisa.birds.model;
+} 
