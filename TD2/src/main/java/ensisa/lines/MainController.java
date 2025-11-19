7d591857d7fc1cae2df98c6b16fb735f90e50772
Gabrieleirbag1 <gabrielgarrone670@gmail.com>
2025-11-19 11:31:30 +0100

    refactor: reorganize MainController for improved structure and clarity

--- TD2/src/main/java/ensisa/lines/MainController.java
+++ TD2/src/main/java/ensisa/lines/MainController.java
@@ -1,22 +1,51 @@
 package ensisa.lines;
 
 import ensisa.lines.model.Document;
-import javafx.application.Platform;
+import ensisa.lines.model.LinesEditor;
+import ensisa.lines.model.StraightLine;
+import javafx.collections.ListChangeListener;
 import javafx.fxml.FXML;
 import javafx.scene.layout.Pane;
 
 public class MainController {
-    private final Document document = new Document();
-    
+    private final Document document;
+    private LinesEditor linesEditor;
     @FXML
     public Pane editorPane;
 
-    @FXML
-    private void quitMenuAction() {
-        Platform.exit();
+    public MainController() {
+        document = new Document();
     }
 
-    public Document getDocument() {
-        return document;
+    public void initialize() {
+        linesEditor = new LinesEditor(editorPane);
+        observeDocument();
+        StraightLine l = new StraightLine();
+        l.setStartX(10);
+        l.setStartY(20);
+        l.setEndX(300);
+        l.setEndY(60);
+        document.getLines().add(l);
+    }
+
+    private void observeDocument() {
+        document.getLines().addListener(new ListChangeListener<StraightLine>() {
+            public void onChanged(ListChangeListener.Change<? extends StraightLine> c) {
+                while (c.next()) {
+                    // Des lignes ont été supprimées du modèle
+                    for (StraightLine line : c.getRemoved()) {
+                        linesEditor.removeLine(line);
+                    }
+                    // Des lignes ont été ajoutées au modèle
+                    for (StraightLine line : c.getAddedSubList()) {
+                        linesEditor.createLine(line);
+                    }
+                }
+            }
+        });
+    }
+
+    public LinesEditor getLinesEditor() {
+        return linesEditor;
     }
 }
