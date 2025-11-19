c463c41be86a5dd2f1da63c2f3431c8c9a9694b4
Gabrieleirbag1 <gabrielgarrone670@gmail.com>
2025-11-19 11:25:20 +0100

    feat: add Document and LinesEditor classes for managing and rendering lines in the editor

--- TD2/src/main/java/ensisa/lines/MainController.java
+++ TD2/src/main/java/ensisa/lines/MainController.java
@@ -1,20 +1,22 @@
 package ensisa.lines;
 
-import java.io.IOException;
-
+import ensisa.lines.model.Document;
 import javafx.application.Platform;
 import javafx.fxml.FXML;
+import javafx.scene.layout.Pane;
 
 public class MainController {
-
+    private final Document document = new Document();
+    
     @FXML
-    private void onHelloButtonClick() throws IOException {
-        System.out.println("Hello World!");
-    }
+    public Pane editorPane;
 
     @FXML
     private void quitMenuAction() {
         Platform.exit();
     }
 
+    public Document getDocument() {
+        return document;
+    }
 }
---
+++ TD2/src/main/java/ensisa/lines/model/LinesEditor.java
@@ -0,0 +1,38 @@
+package ensisa.lines.model;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import javafx.scene.layout.Pane;
+import javafx.scene.shape.Line;
+
+public class LinesEditor {
+    private final Pane editorPane;
+    private final Map<StraightLine, Line> lines;
+
+    public LinesEditor(Pane editorPane) {
+        this.editorPane = editorPane;
+        lines = new HashMap<>();
+    }
+
+    public void createLine(StraightLine straightLine) {
+        Line line = new Line();
+        lines.put(straightLine, line);
+        bind(line, straightLine);
+        editorPane.getChildren().add(line);
+    }
+
+    public void removeLine(StraightLine straightLine) {
+        Line line = lines.remove(straightLine);
+        editorPane.getChildren().remove(line);
+    }
+
+    private void bind(Line line, StraightLine straightLine) {
+        line.startXProperty().bind(straightLine.startXProperty());
+        line.startYProperty().bind(straightLine.startYProperty());
+        line.endXProperty().bind(straightLine.endXProperty());
+        line.endYProperty().bind(straightLine.endYProperty());
+        line.strokeWidthProperty().bind(straightLine.strokeWidthProperty());
+        line.strokeProperty().bind(straightLine.colorProperty());
+    }
+}
