a65c1d87edeeaa877795f17ff6321542e2e6a362
Gabrieleirbag1 <gabrielgarrone670@gmail.com>
2025-11-19 10:41:36 +0100

    init main controller

---
+++ TD2/src/main/java/ensisa/lines/MainController.java
@@ -0,0 +1,12 @@
+package ensisa.lines;
+
+import java.io.IOException;
+import javafx.fxml.FXML;
+
+public class MainController {
+
+    @FXML
+    private void onHelloButtonClick() throws IOException {
+        System.out.println("Hello World!");
+    }
+}
